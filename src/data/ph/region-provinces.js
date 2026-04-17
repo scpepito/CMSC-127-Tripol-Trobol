cb8cf627761ac1746fd65c105c5f51a38ed190f8
scpepito <scpepito@up.edu.ph>
2026-04-17 15:32:52 +0800

    feat: add regions/provinces/cities for location dropdown options

---
+++ src/data/ph/region-provinces.js
@@ -0,0 +1,124 @@
+// Generated from PSGC Cloud (https://psgc.cloud/api).
+// Generated at: 2026-04-16T17:30:59.123Z
+// Note: PSGC may change; re-run scripts/generate-ph-locations.mjs to update.
+
+
+export const PH_REGION_TO_PROVINCES = {
+  "Bangsamoro Autonomous Region In Muslim Mindanao (BARMM)": [
+    "Basilan",
+    "Lanao del Sur",
+    "Maguindanao del Norte",
+    "Maguindanao del Sur",
+    "Sulu",
+    "Tawi-Tawi"
+  ],
+  "Cordillera Administrative Region (CAR)": [
+    "Abra",
+    "Apayao",
+    "Benguet",
+    "Ifugao",
+    "Kalinga",
+    "Mountain Province"
+  ],
+  "MIMAROPA Region": [
+    "Marinduque",
+    "Occidental Mindoro",
+    "Oriental Mindoro",
+    "Palawan",
+    "Romblon"
+  ],
+  "National Capital Region (NCR)": [
+    "Metro Manila"
+  ],
+  "Region I (Ilocos Region)": [
+    "Ilocos Norte",
+    "Ilocos Sur",
+    "La Union",
+    "Pangasinan"
+  ],
+  "Region II (Cagayan Valley)": [
+    "Batanes",
+    "Cagayan",
+    "Isabela",
+    "Nueva Vizcaya",
+    "Quirino"
+  ],
+  "Region III (Central Luzon)": [
+    "Aurora",
+    "Bataan",
+    "Bulacan",
+    "Nueva Ecija",
+    "Pampanga",
+    "Tarlac",
+    "Zambales"
+  ],
+  "Region IV-A (CALABARZON)": [
+    "Batangas",
+    "Cavite",
+    "Laguna",
+    "Quezon",
+    "Rizal"
+  ],
+  "Region IX (Zamboanga Peninsula)": [
+    "Zamboanga del Norte",
+    "Zamboanga del Sur",
+    "Zamboanga Sibugay"
+  ],
+  "Region V (Bicol Region)": [
+    "Albay",
+    "Camarines Norte",
+    "Camarines Sur",
+    "Catanduanes",
+    "Masbate",
+    "Sorsogon"
+  ],
+  "Region VI (Western Visayas)": [
+    "Aklan",
+    "Antique",
+    "Capiz",
+    "Guimaras",
+    "Iloilo",
+    "Negros Occidental"
+  ],
+  "Region VII (Central Visayas)": [
+    "Bohol",
+    "Cebu",
+    "Negros Oriental",
+    "Siquijor"
+  ],
+  "Region VIII (Eastern Visayas)": [
+    "Biliran",
+    "Eastern Samar",
+    "Leyte",
+    "Northern Samar",
+    "Samar",
+    "Southern Leyte"
+  ],
+  "Region X (Northern Mindanao)": [
+    "Bukidnon",
+    "Camiguin",
+    "Lanao del Norte",
+    "Misamis Occidental",
+    "Misamis Oriental"
+  ],
+  "Region XI (Davao Region)": [
+    "Davao de Oro",
+    "Davao del Norte",
+    "Davao del Sur",
+    "Davao Occidental",
+    "Davao Oriental"
+  ],
+  "Region XII (SOCCSKSARGEN)": [
+    "Cotabato",
+    "Sarangani",
+    "South Cotabato",
+    "Sultan Kudarat"
+  ],
+  "Region XIII (Caraga)": [
+    "Agusan del Norte",
+    "Agusan del Sur",
+    "Dinagat Islands",
+    "Surigao del Norte",
+    "Surigao del Sur"
+  ]
+}
---
+++ src/data/ph/regions.js
@@ -0,0 +1,24 @@
+// Generated from PSGC Cloud (https://psgc.cloud/api).
+// Generated at: 2026-04-16T17:30:59.123Z
+// Note: PSGC may change; re-run scripts/generate-ph-locations.mjs to update.
+
+
+export const PH_REGIONS = [
+  "Bangsamoro Autonomous Region In Muslim Mindanao (BARMM)",
+  "Cordillera Administrative Region (CAR)",
+  "MIMAROPA Region",
+  "National Capital Region (NCR)",
+  "Region I (Ilocos Region)",
+  "Region II (Cagayan Valley)",
+  "Region III (Central Luzon)",
+  "Region IV-A (CALABARZON)",
+  "Region IX (Zamboanga Peninsula)",
+  "Region V (Bicol Region)",
+  "Region VI (Western Visayas)",
+  "Region VII (Central Visayas)",
+  "Region VIII (Eastern Visayas)",
+  "Region X (Northern Mindanao)",
+  "Region XI (Davao Region)",
+  "Region XII (SOCCSKSARGEN)",
+  "Region XIII (Caraga)"
+]
